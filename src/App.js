019b525fa18efedc75089d9d2890b3db27957ec6
jordanovvvv <jordanovsimeon1@gmail.com>
2023-05-17 14:35:05 +0200

    Updated some stuff

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {useState} from "react";
-import axios from 'axios';
+
 
 function App() {
 
-  const [inputFile, setInputFile] = useState(null);
-  const [result, setResult] = useState(null);
-  const [processingTime, setProcessingTime] = useState(null);
+    const [inputFile, setInputFile] = useState(null);
+    const [result, setResult] = useState(null);
+    const [processingTime, setProcessingTime] = useState(null);
 
-  const handleFileUpload = (event) => {
-    setInputFile(event.target.files[0]);
-  };
+    const handleFileUpload = (event) => {
+        setInputFile(event.target.files[0]);
+    };
 
-  const startCalculation = async (event) => {
-    event.preventDefault();
-    const startTime = performance.now();
-    const formData = new FormData();
-    formData.append('inputFile', inputFile);
-    console.log(inputFile)
+    const startCalculation = async (event) => {
+        event.preventDefault();
+        const startTime = performance.now();
+        const formData = new FormData();
+        formData.append('inputFile', inputFile);
+        console.log(inputFile)
 
-    try {
-      const response = await fetch("http://localhost:3000/sort", {
-        method: 'POST',
-        body: formData,
-        mode: 'no-cors',
-      });
+        fetch("http://localhost:3000/sort", {
+            method: 'POST',
+            body: formData,
+        })
+            .then((res) => res.blob())
+            .then((data) => {
+                setResult(data);
+                setProcessingTime(performance.now() - startTime);
+            })
+            .catch((err) => console.error(err));
 
-      const data = await response.text();
-      console.log(data);
-      setResult(data);
-      setProcessingTime(performance.now() - startTime);
-    } catch (error) {
-      console.error(error);
-    }
+    };
 
-  };
 
-  const handleDownload = (event) => {
-    event.preventDefault();
-    const file = new Blob([result], {
-      type: 'text/plain'
-    });
-    const fileURL = URL.createObjectURL(file);
-    const link = document.createElement('a');
-    link.href = fileURL;
-    link.download = 'result.txt';
-    link.click();
+
+    const handleDownload = () => {
+    fetch('http://localhost:3000/download')
+        .then((response) => response.blob())
+        .then((blob) => {
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'sorted-files.txt';
+            link.click();
+        })
+        .catch((err) => console.log(err));
   };
 
   return (
       <div className="App">
-        <input type="file" name="inputFile" id="input" onChange={handleFileUpload} />
+        <input type="file" name="inputFile" id="input" multiple onChange={handleFileUpload} />
         <button className="App-button" onClick={startCalculation}>Start Calculation</button>
         <button className="App-button" onClick={handleDownload}>Download Result</button>
         <p>Processing Time: {processingTime}ms</p>
